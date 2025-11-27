011df217e49214c183decd39639a109f2e6bc363
JustMelih <JustMelih@users.noreply.github.com>
2025-11-27 14:49:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Portfolio;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+var app = builder.Build();
+app.UseRouting();
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+app.Run();
