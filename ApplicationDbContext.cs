c830a794b7b2efb12a7e0240ee640cddd4da304d
JustMelih <JustMelih@users.noreply.github.com>
2025-11-27 14:49:49 +0300

    Frontend completed using Tailwind.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Models;
+namespace Portfolio
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Project> Projects { get; set; } = null!;
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminIndex()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
